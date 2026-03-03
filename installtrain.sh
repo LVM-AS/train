3b51ee08b825f915aafbdaa718c5ecb988f0922d
chrisronaasen <chrisronaasen@gmail.com>
2026-03-03 19:41:56 +0100

    new file:   default.sh 	modified:   installtrain.sh

--- installtrain.sh
+++ installtrain.sh
@@ -22,22 +22,35 @@ echo "."
 echo "."
 echo "."
 
-# echo "downloading LTX-2 checkpoint model from Hugging Face"
-# cd /workspace/train/models/checkpoint
-# hf download Lightricks/LTX-2 ltx-2-19b-distilled.safetensors --local-dir .
-# rm -rf .cache/
-# echo "finished downloading LTX-2 checkpoint model from Hugging Face"
+echo "downloading LTX-2 checkpoint model from Hugging Face"
+mkdir -p /workspace/ComfyUI/models/checkpoints/LTX
+cd /workspace/ComfyUI/models/checkpoints/LTX
+hf download Lightricks/LTX-2 ltx-2-19b-distilled-fp8.safetensors --local-dir .
+rm -rf .cache/
+echo "finished downloading LTX-2 checkpoint model from Hugging Face"
 
-# echo "downloading LTX-2 text encoder from Hugging Face"
-# cd /workspace/train/models/text_encoder
-# hf download p-e-w/gemma-3-12b-it-heretic --local-dir .
-# rm -rf .cache/
-# echo "finished downloading LTX-2 text encoder from Hugging Face"
+echo "downloading LTX-2 text encoder from Hugging Face"
+mkdir -p /workspace/ComfyUI/models/text_encoders/LTX
+cd /workspace/ComfyUI/models/text_encoders/LTX
+hf download p-e-w/gemma-3-12b-it-heretic-v2 --local-dir .
+rm -rf .cache/
+echo "finished downloading LTX-2 text encoder from Hugging Face"
+
+echo "downloading LTX-2 text encoder config files from Hugging Face"
+wget https://huggingface.co/google/gemma-3-12b-it-qat-q4_0-unquantized/resolve/main/added_tokens.json
+wget https://huggingface.co/google/gemma-3-12b-it-qat-q4_0-unquantized/resolve/main/chat_template.json
+wget https://huggingface.co/google/gemma-3-12b-it-qat-q4_0-unquantized/resolve/main/preprocessor_config.json
+wget https://huggingface.co/google/gemma-3-12b-it-qat-q4_0-unquantized/resolve/main/processor_config.json
+wget https://huggingface.co/google/gemma-3-12b-it-qat-q4_0-unquantized/resolve/main/tokenizer.model
+rm -rf tokenizer_config.json
+wget https://huggingface.co/p-e-w/gemma-3-12b-it-heretic-v2/resolve/main/tokenizer_config.json
+echo "finished downloading LTX-2 text encoder config files from Hugging Face"
 
 echo "downloading training repo from GitHub"
 cd /workspace/
-git clone https://github.com/fqwqwf/train
+git clone https://github.com/LVM-AS/train
 cd /workspace/train/LTX-2
+echo "finished downloading training repo from GitHub"
 
 echo "installing base requirements for training"
 uv sync
@@ -62,11 +75,6 @@ echo "finished installing base requirements for training"
 #echo "uninstalling xformers if present"
 #uv pip uninstall -y xformers
 #echo "finished uninstalling xformers"
-
-echo "cleaning up uv & pip cache to save space"
-uv cache prune
-pip cache purge
-echo "finished cleaning up uv & pip cache"
 
 echo "cleaning up uv & pip cache to save space"
 pip cache purge

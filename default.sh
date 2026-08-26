e53eae065d0a6af1583ac6e6cb8ce41855229762
LVM-AS <chrisronaasentw2@gmail.com>
2026-08-26 20:25:34 +0200

    modified:   default.sh

--- default.sh
+++ default.sh
@@ -309,6 +309,7 @@ git clone https://github.com/alisson-anjos/ComfyUI-BFSNodes
 git clone https://github.com/liconstudio/ComfyUI-Licon-MSR
 git clone https://github.com/blepping/ComfyUI-bleh
 git clone https://github.com/LBH-123-AI/Comfyui_Minimax_h3_latent_Upscaler
+git clone https://github.com/imbutus/ComfyUI-MiniMaxDirector
 # git clone https://github.com/Lightricks/ComfyUI-LTXVideo
 echo "- - - - - - - - - - --  DONE -- - - - - - - - - - - -"
 
@@ -371,6 +372,7 @@ pip install -r /workspace/ComfyUI/custom_nodes/ComfyUI-BFSNodes/requirements.txt
 pip install -r /workspace/ComfyUI/custom_nodes/ComfyUI-Licon-MSR/requirements.txt
 pip install -r /workspace/ComfyUI/custom_nodes/ComfyUI-bleh/requirements.txt
 pip install -r /workspace/ComfyUI/custom_nodes/Comfyui_Minimax_h3_latent_Upscaler/requirements.txt
+# pip install -r /workspace/ComfyUI/custom_nodes/ComfyUI-MiniMaxDirector/requirements.txt
 # pip install -r /workspace/ComfyUI/custom_nodes/ComfyUI-LTXVideo/requirements.txt
 echo "- - - - - - - - - - --  DONE -- - - - - - - - - - - -"
 
